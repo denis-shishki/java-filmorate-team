e8fa0221a2b337eb35387ce0a5a25a79253b9972
tr33hz <120664451+tr33hz@users.noreply.github.com>
2023-10-06 16:30:04 +0300

    Добавлен GET эндпоинт для получения отзывов по id фильма в ReviewsController. В методе сервиса добавлены логи на получение GET запроса и основана логика получения отзывов в DAO классе ReviewDbStorage.

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exceptions.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ReviewNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.ReviewStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +29,7 @@ public class ReviewService {
         filmStorage.findFilm(review.getFilmId())
                 .orElseThrow(() -> new FilmNotFoundException("Попытка оставить отзыв несуществующему фильму"));
 
-        userStorage.findUser(review.getId())
+        userStorage.findUser(review.getUserId())
                 .orElseThrow(() -> new UserNotFoundException("Попытка оставить отзыв несуществующим пользователем"));
         return reviewStorage.create(review);
     }
@@ -54,4 +57,29 @@ public class ReviewService {
                 .orElseThrow(() -> new ReviewNotFoundException("Запрашиваемый отзыв не существует"));
         reviewStorage.delete(id);
     }
+
+    public List<Review> getAllReviews() {
+        log.info("Поступил запрос на получение всех отзывов в приложении");
+
+        return reviewStorage.getAll();
+    }
+
+    public List<Review> getAllReviewsByFilm(Integer filmId, int count) {
+        log.info("Поступил запрос на получения отзывов к фильму = {}, в количестве = {}", filmId, count);
+
+        Film film = filmStorage.findFilm(filmId)
+                .orElseThrow(() -> new FilmNotFoundException("Запрашиваемый фильм не существует"));
+
+        List<Review> allReviewsByFilm;
+        try {
+            allReviewsByFilm = reviewStorage.findReviewsByFilm(filmId, count);
+            log.info("Запрос на получение отзывов к фильму = {}, в количестве = {} успешно обработан", filmId, count);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Произошла ошибка при поиске отзывов по id фильма");
+        }
+
+        return allReviewsByFilm;
+    }
+
+
 }
