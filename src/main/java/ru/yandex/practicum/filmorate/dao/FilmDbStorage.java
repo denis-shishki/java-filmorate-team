2eee6a96f4163f5cf3776e41cb7e9282f8b5c513
Denis <schishckin.dennis@yandex.ru>
2023-10-10 22:29:30 +0400

    Перенес SortBy в пакет constants

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -9,7 +9,7 @@ import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.model.SortBy;
+import ru.yandex.practicum.filmorate.constants.SortBy;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.sql.PreparedStatement;
